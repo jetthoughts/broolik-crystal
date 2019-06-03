c2f6a121c9f36c35e3f357d70c542eabe4705ce2
Paul Nikitochkin <pftg@users.noreply.github.com>
2019-06-04 02:15:35 +0300

    Removes redundant example

--- examples/single_link_verification.cr
+++ examples/single_link_verification.cr
@@ -10,19 +10,3 @@ link = client.create_link({
 
 puts "Link validation status is: #{link.status}"
 
-batch = client.create_batch({"file" => "spec/fixtures/urls_example.csv"})
-puts "Batch is #{batch.status}"
-
-until batch.report_completed?
-  sleep 15
-
-  batch = client.find_batch(batch.id)
-
-  puts "Batch is #{batch.status}"
-end
-
-puts "Report located on #{batch.links_report}"
-
-HTTP::Client.get(batch.links_report.not_nil!) do |response|
-  puts response.body
-end
