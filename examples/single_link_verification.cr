810faf20860035362974f1d6522b57b049cd7d33
Paul Nikitochkin <pftg@users.noreply.github.com>
2019-06-04 19:18:32 +0300

    Auto-fix code format

--- examples/single_link_verification.cr
+++ examples/single_link_verification.cr
@@ -3,9 +3,9 @@ require "../src/broolik"
 client = Broolik.client("https://broolik.tk")
 
 link = client.create_link({
-# "country" => "UKR",
-  "url"     => "http://jetthoughts.com",
-  "device"  => "iphone",
+  # "country" => "UKR",
+  "url"    => "http://jetthoughts.com",
+  "device" => "iphone",
 })
 
 puts "Link validation status for http://jetthoughts.com is: #{link.status}"
