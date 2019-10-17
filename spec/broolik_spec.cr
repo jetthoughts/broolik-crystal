72b6a0b562b88c810341558842ab9ac82bc65cd9
Michael Nikitochkin <michael@jetthoughts.com>
2019-10-17 08:26:35 +0200

    Use broolik.ml

--- spec/broolik_spec.cr
+++ spec/broolik_spec.cr
@@ -2,8 +2,8 @@ require "./spec_helper"
 
 describe Broolik do
   describe ".client" do
-    it "use https://broolik.tk/ as default endpoint" do
-      Broolik.client.endpoint.should eq "https://broolik.tk"
+    it "use https://broolik.ml/ as default endpoint" do
+      Broolik.client.endpoint.should eq "https://broolik.ml"
     end
   end
 end
